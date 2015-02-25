2bd3a87f0483c38228da2908064a6d0737833b05
Michael Kaminsky <mkaminsky11@gmail.com>
2015-02-24 21:09:11 -0500

    working on css script

--- docs/css/script.js
+++ docs/css/script.js
@@ -8,10 +8,23 @@ request({
 	$ = cheerio.load(body);
 	var main = $(".index > ol > li > ol > li > a").each(function(index){
 		var href = "https://developer.mozilla.org" + $(this).attr("href");
-		console.log(href);
-		
+
 		var base = $(this).html().replace("<code>","<!--next-->\n<h2>").replace("</code>","</h2>");
-		console.log(base);
+        request({
+           uri: href
+        }, function(err, res, bdy){
+            $ = cheerio.load(bdy);
+            try{
+                var wiki = $("#wikiArticle").html().split("<h2 id=\"Examples\">Examples</h2>")[0];
+
+                wiki.replace(/class="(.*?)"/g, "");
+                wiki.replace(/id="(.*?)"/g, "");
+                wiki.replace(/href="(.*?)"/g, "");
+
+                console.log(href);
+            }
+            catch(e){}
+        });
 	});
 });
 
