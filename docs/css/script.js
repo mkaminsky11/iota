6ed553d8b3b64105fdd805eccfc5e2804dbd8b36
Michael Kaminsky <mkaminsky11@gmail.com>
2015-03-04 14:30:15 -0500

    still working on pulling css docs

--- docs/css/script.js
+++ docs/css/script.js
@@ -5,25 +5,29 @@ var cheerio = require('cheerio');
 request({
 	uri: "https://developer.mozilla.org/en-US/docs/Web/CSS/Reference"
 }, function(err, res, body){
-	$ = cheerio.load(body);
-	var main = $(".index > ol > li > ol > li > a").each(function(index){
+		$ = cheerio.load(body);
+		var main = $(".index > ol > li > ol > li > a").each(function(index){
 		var href = "https://developer.mozilla.org" + $(this).attr("href");
 
 		var base = $(this).html().replace("<code>","<!--next-->\n<h2>").replace("</code>","</h2>");
         request({
            uri: href
         }, function(err, res, bdy){
-            $ = cheerio.load(bdy);
             try{
-                var wiki = $("#wikiArticle").html().split("<h2 id=\"Examples\">Examples</h2>")[0];
+								//$ = cheerio.load(bdy);
+                //var wiki = $("#wikiArticle").html().split("<h2 id=\"Examples\">Examples</h2>")[0];
+								var wiki = bdy.split("<article id=\"wikiArticle\">")[1].split("</article>")[0].split("<h2 id=\"Examples\">Examples</h2>")[0];
 
-                wiki.replace(/class="(.*?)"/g, "");
+              	wiki.replace(/class="(.*?)"/g, "");
                 wiki.replace(/id="(.*?)"/g, "");
                 wiki.replace(/href="(.*?)"/g, "");
 
-                console.log(href);
+                var file = "css_" + href.split("/CSS/")[1];
+								console.log(file);
             }
-            catch(e){}
+            catch(e){
+							console.log("error:" + e + ":" + href);
+						}
         });
 	});
 });
